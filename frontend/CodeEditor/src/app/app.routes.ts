a82583f4f316958454e4952423f9a83edad3b64f
Reclaimers <floran.nicoletti38@gmail.com>
2026-01-28 20:14:57 +0100

    Setting up routes, landing page, not found page

--- frontend/CodeEditor/src/app/app.routes.ts
+++ frontend/CodeEditor/src/app/app.routes.ts
@@ -1,3 +1,20 @@
 import { Routes } from '@angular/router';
+import { Landing } from './components/landing/landing';
+import { NotFound } from './components/not-found/not-found';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { 
+    path: '', 
+    redirectTo: '/landing', 
+    pathMatch: 'full' 
+  },
+  { 
+    path: 'landing', 
+    component: Landing 
+  },
+  // Not found page 
+  { 
+    path: '**', 
+    component: NotFound
+  }
+];
