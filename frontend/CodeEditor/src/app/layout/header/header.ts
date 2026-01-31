ed11564c548529622454781cbfd68f518c8b4813
Reclaimers <floran.nicoletti38@gmail.com>
2026-01-31 20:12:09 +0100

    Add SCSS Add header Add Material 3

--- frontend/CodeEditor/src/app/layout/header/header.ts
+++ frontend/CodeEditor/src/app/layout/header/header.ts
@@ -1,11 +1,30 @@
-import { Component } from '@angular/core';
+import { Component, signal } from '@angular/core';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIcon } from '@angular/material/icon';
+import { RouterLink } from '@angular/router';
+import { HeaderLogin } from '../../components/layout/header-login/header-login';
 
 @Component({
   selector: 'app-header',
-  imports: [],
+  imports: [
+    HeaderLogin, 
+    MatToolbarModule, 
+    RouterLink, 
+    MatButtonModule,
+    MatIcon
+  ],
   templateUrl: './header.html',
-  styleUrl: './header.css',
+  styleUrl: './header.scss',
 })
 export class Header {
+  isMenuOpen = false;
 
+  toggleMenu() {
+    this.isMenuOpen = !this.isMenuOpen;
+  }
+
+  closeMenu() {
+    this.isMenuOpen = false;
+  }
 }
