3260a2ccdf5df57bb3e72fd74a5c8329315070ed
Reclaimers <floran.nicoletti38@gmail.com>
2026-01-31 14:05:53 +0100

    Authentication done Setting up layout WIP Header

--- frontend/CodeEditor/src/app/app.ts
+++ frontend/CodeEditor/src/app/app.ts
@@ -1,10 +1,9 @@
 import { Component, signal } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
-import { CodeEditor } from './features/components/code-editor/code-editor';
+import { Layout } from './layout/layout';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [Layout],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
