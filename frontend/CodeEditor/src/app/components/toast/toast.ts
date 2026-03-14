326b04834c50ec3e83deeb1a893c36dbb400450d
Reclaimers <floran.nicoletti38@gmail.com>
2026-03-14 15:42:25 +0100

    Toast animation + add warning css class for toast

--- frontend/CodeEditor/src/app/components/toast/toast.ts
+++ frontend/CodeEditor/src/app/components/toast/toast.ts
@@ -1,4 +1,4 @@
-import { Component, computed, input } from '@angular/core';
+import { Component, computed, input, OnInit, output, signal } from '@angular/core';
 import { NgClass } from  '@angular/common';
 
 @Component({
@@ -7,12 +7,20 @@ import { NgClass } from  '@angular/common';
   templateUrl: './toast.html',
   styleUrl: './toast.scss',
 })
-export class Toast {
+export class Toast implements OnInit  {
+  ngOnInit(): void {
+    setTimeout(() => {
+      this.dismiss.emit();
+    }, 1000)
+  }
+
   message = input.required<string>();
   type = input.required<'success' | 'error' | 'warning'>();
+  isVisible = signal(true);
+
+  dismiss = output<void>();
 
   toast_class =  computed(() => {
-    console.log("Computed signal");
     switch (this.type()) {
       case 'success': return 'toast-container-success';
       case 'error':   return 'toast-container-error';
@@ -20,4 +28,10 @@ export class Toast {
       default:        return '';
     }
   });
+
+  leave() {
+    console.log("Leave");
+    this.isVisible.set(false);
+  }
+
 }
--- frontend/CodeEditor/src/app/core/services/toast/toast.service.ts
+++ frontend/CodeEditor/src/app/core/services/toast/toast.service.ts
@@ -1,20 +1,39 @@
-import { Injectable, signal } from '@angular/core';
+import { ApplicationRef, createComponent, EnvironmentInjector, inject, Injectable, inputBinding, outputBinding, signal } from '@angular/core';
+import { Toast } from '../../../components/toast/toast';
 
 @Injectable({
   providedIn: 'root',
 })
 export class ToastService {
+  private appRef = inject(ApplicationRef);
+  private injector = inject(EnvironmentInjector);
+  
   message = signal<string>('');
   type = signal<'success' | 'error' | 'warning'>('success');
 
 
   show(message: string, type: 'success' | 'error' | 'warning') {
-    console.log("Set values");
-    this.message.set(message);
-    this.type.set(type);
-    // setTimeout(() => {
-    //   this.clear();
-    // }, 1500)
+    const container = document.getElementById('toasts-container')!;
+    const host = document.createElement('div');
+
+    const ref = createComponent(Toast, {
+      environmentInjector: this.injector,
+      hostElement: host,
+      bindings: [
+        inputBinding('message', () => message),
+        inputBinding('type', () => type),
+        outputBinding('dismiss', () => {
+          ref.instance.leave();
+          setTimeout(() => {
+            this.appRef.detachView(ref.hostView);
+            ref.destroy();
+          }, 1500)
+        }),
+      ],
+    });
+
+    this.appRef.attachView(ref.hostView);
+    container.appendChild(host);
   }
 
   clear(){
