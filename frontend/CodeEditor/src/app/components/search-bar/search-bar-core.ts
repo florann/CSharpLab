7c45e9b6c75eb7c3c4e186cbb350b59b05da0032
Reclaimers <floran.nicoletti38@gmail.com>
2026-03-18 14:33:35 +0100

    WIP search bar

---
+++ frontend/CodeEditor/src/app/components/search-bar/search-bar-core.ts
@@ -0,0 +1,27 @@
+import { Component, OnDestroy, OnInit, signal } from '@angular/core';
+import { filter, fromEvent, Subscription } from 'rxjs';
+
+@Component({
+  selector: 'app-search-bar-core',
+  imports: [],
+  templateUrl: './search-bar-core.html',
+  styleUrl: './search-bar-core.scss',
+})
+export class SearchBarCore implements OnInit, OnDestroy {
+  isVisible = signal(false);
+  private sub!: Subscription;
+
+  ngOnInit(): void {
+    this.sub = fromEvent<KeyboardEvent>(document, 'keydown').pipe(
+      filter(e => e.altKey && e.key === 'r') 
+    ).subscribe(e => {
+      console.log("Event fired");
+      e.preventDefault();
+      this.isVisible.set(!this.isVisible());
+    })
+  }
+  
+  ngOnDestroy(): void {
+    this.sub.unsubscribe();
+  }
+}
--- frontend/CodeEditor/src/app/layout/layout.ts
+++ frontend/CodeEditor/src/app/layout/layout.ts
@@ -2,12 +2,12 @@ import { Component, inject, Inject } from '@angular/core';
 import { Header } from './header/header';
 import { Footer } from './footer/footer';
 import { RouterOutlet } from "@angular/router";
-import { Toast } from "../components/toast/toast";
 import { ToastService } from '../core/services/toast/toast.service';
+import { SearchBarCore } from "../components/search-bar/search-bar-core";
 
 @Component({
   selector: 'app-layout',
-  imports: [Header, Footer, RouterOutlet, Toast],
+  imports: [Header, Footer, RouterOutlet, SearchBarCore],
   templateUrl: './layout.html',
   styleUrl: './layout.scss',
 })
