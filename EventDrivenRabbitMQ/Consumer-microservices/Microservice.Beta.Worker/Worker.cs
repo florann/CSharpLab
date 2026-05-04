f93d8a8f03327fe0c339febeec8e49a4ebc72b0e
Reclaimers <floran.nicoletti38@gmail.com>
2026-05-05 00:03:17 +0200

    [Feat] - Consumer worker service correction + docker file

--- EventDrivenRabbitMQ/Consumer-microservices/Microservice.Beta.Worker/Worker.cs
+++ EventDrivenRabbitMQ/Consumer-microservices/Microservice.Beta.Worker/Worker.cs
@@ -17,6 +17,23 @@ namespace Microservice.Beta.Worker
 
         protected override async Task ExecuteAsync(CancellationToken stoppingToken)
         {
+            var consumer = new AsyncEventingBasicConsumer(channel);
+
+            consumer.ReceivedAsync += (model, ea) =>
+            {
+                var body = ea.Body.ToArray();
+                var message = Encoding.UTF8.GetString(body);
+                Console.WriteLine($"Received: {message}");
+
+                Task.Delay(2000);
+
+                return Task.CompletedTask;    
+            };
+
+            await channel.BasicQosAsync(0, 1, false, stoppingToken);
+
+            await channel.BasicConsumeAsync("alpha", true, consumer, cancellationToken: stoppingToken);
+
             while (!stoppingToken.IsCancellationRequested)
             {
                 if (logger.IsEnabled(LogLevel.Information))
@@ -24,21 +41,7 @@ namespace Microservice.Beta.Worker
                     logger.LogInformation("Worker running at: {time}", DateTimeOffset.Now);
                 }
 
-                var consumer = new AsyncEventingBasicConsumer(channel);
-
-                consumer.ReceivedAsync += (model, ea) =>
-                {
-                    var body = ea.Body.ToArray();
-                    var message = Encoding.UTF8.GetString(body);
-                    Console.WriteLine($"Received: {message}");
-                    return Task.CompletedTask;    
-                };
-
-                var result = await channel.BasicConsumeAsync("alpha", true, consumer);
-
-                Console.WriteLine($"Result: {result}");
-
-                await Task.Delay(2000, stoppingToken);
+                await Task.Delay(1000, stoppingToken);
             }
         }
 
