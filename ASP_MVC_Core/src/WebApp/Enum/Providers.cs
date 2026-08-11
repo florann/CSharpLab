a589c7c41ce2f579869cecd8773d51d2dede7d97
Reclaimers <floran.nicoletti38@gmail.com>
2026-08-11 11:34:45 +0200

    [Feat] - Add flex configuration postgre / sql server

---
+++ ASP_MVC_Core/src/WebApp/Enum/Providers.cs
@@ -0,0 +1,8 @@
+﻿namespace WebApp.Enum
+{
+    public enum Providers
+    {
+        Postgre = 1,
+        SqlServer = 2
+    }
+}
--- ASP_MVC_Core/src/WebApp/Extensions/WebApplicationBuilderExtension.cs
+++ ASP_MVC_Core/src/WebApp/Extensions/WebApplicationBuilderExtension.cs
@@ -1,14 +1,48 @@
-﻿namespace WebApp.Extensions
+﻿using Microsoft.EntityFrameworkCore;
+using WebApp.Enum;
+using WebApp.Infrastructure.Persistence;
+
+namespace WebApp.Extensions
 {
     public static class WebApplicationBuilderExtension
     {
-        extension(WebApplicationBuilder webApplicationBuilder) { 
+        extension(WebApplicationBuilder webApplicationBuilder) {
             public WebApplicationBuilder ConfigureDatabaseContext()
             {
-                webApplicationBuilder.Configuration.
+                var provider = webApplicationBuilder.Configuration["DatabaseProvider"];
+                if (string.IsNullOrEmpty(provider))
+                    throw new ApplicationException("No provider was configured");
+
+                var connectionString = webApplicationBuilder.Configuration.GetSection("ConnectionStrings")[provider];
+                if (string.IsNullOrEmpty(connectionString))
+                    throw new ApplicationException("No connection string for provider : " + provider );
+
+                var providerEnum = GetProvider(provider);
+
+                webApplicationBuilder.Services.AddDbContext<AppDbContext>(options =>
+                {
+                    switch (providerEnum)
+                    {
+                        case Providers.Postgre:
+                            options.UseNpgsql(connectionString);
+                            return;
+                        case Providers.SqlServer:
+                            options.UseSqlServer(connectionString);
+                            return;
+                        default:
+                            throw new ApplicationException("Provider not found");
+                    }
+                });
 
                 return webApplicationBuilder;
             }
+
+            private static Providers GetProvider(string provider) => provider switch
+            {
+                "Postgre" => Providers.Postgre,
+                "SqlServer" => Providers.SqlServer,
+                _ => throw new ApplicationException("Could not deduce provider enum value")
+            };
         }
     }
 }
