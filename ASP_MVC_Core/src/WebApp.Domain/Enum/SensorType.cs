4680cdc42d9fc913f81b5b9a508c36d1389fa900
Reclaimers <floran.nicoletti38@gmail.com>
2026-08-17 19:05:23 +0200

    [Feat] - Add sensor enum

---
+++ ASP_MVC_Core/src/WebApp.Domain/Enum/SensorType.cs
@@ -0,0 +1,26 @@
+﻿namespace WebApp.Domain.Enum
+{
+    public enum SensorType
+    {
+        Temperature,
+        Humidity,
+        Pressure,
+        Voltage,
+        Current,
+        Power,
+        Frequency,
+        SoundLevel,
+        Co2,
+        LightLux,
+        UvIndex,
+        SoilMoisture,
+        Vibration,
+        FlowRate,
+        Rpm,
+        Ph,
+        GasConcentration,
+        Turbidity,
+        WindSpeed,
+        Rainfall
+    }
+}
